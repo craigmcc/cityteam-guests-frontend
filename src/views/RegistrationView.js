57858ebe64a7f34c0ddcccec0dc1fc1bdeefcbeb
craigmcc <craigmcc@gmail.com>
2020-10-08 21:44:55 -0700

    Changes: * RegistrationView:   - Use react-bootstrap grid components.   - Make table full width in anticipation     of using modals for registering guests.

--- src/views/RegistrationView.js
+++ src/views/RegistrationView.js
@@ -1,4 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
+import Button from "react-bootstrap/Button"
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Row from "react-bootstrap/Row";
 
 import FacilityClient from "../clients/FacilityClient";
 import TemplateClient from "../clients/TemplateClient";
@@ -13,9 +18,10 @@ const RegistrationView = () => {
     const facilityContext = useContext(FacilityContext);
 
     const [index, setIndex] = useState(-1);
-    const [items, setItems] = useState([]);
+    const [registration, setRegistration] = useState(null);
     const [registrationDate, setRegistrationDate] =
         useState("2020-07-04"); // TODO - "today"
+    const [registrations, setRegistrations] = useState([]);
 
     useEffect(() => {
         retrieveAllItems(registrationDate);
@@ -29,7 +35,7 @@ const RegistrationView = () => {
             .then(response => {
                 console.log("RegistrationView.handleGenerate got (" +
                     JSON.stringify(response.data, ["id", "matNumber", "features"]) + ")");
-                saveItems(response.data);
+                saveRegistrations(response.data);
             })
     }
 
@@ -49,70 +55,76 @@ const RegistrationView = () => {
             .then(response => {
                 console.log("RegistrationView.retrieveAllItems(" +
                     JSON.stringify(response.data, ["id", "matNumber", "features", "guest"]) + ")");
-                saveItems(response.data);
+                saveRegistrations(response.data);
             })
             .catch(e => {
                 console.log(e);
             })
     }
 
-    const saveItems = (items) => {
+    const saveRegistrations = (registrations) => {
         let flattenedItems =
-            withFlattenedObjects(items, "guest");
+            withFlattenedObjects(registrations, "guest");
         for (let flattenedItem of flattenedItems) {
             flattenedItem.matNumberAndFeatures = "" + flattenedItem.matNumber;
             if (flattenedItem.features) {
                 flattenedItem.matNumberAndFeatures += flattenedItem.features;
             }
         }
-        setItems(flattenedItems);
+        setRegistrations(flattenedItems);
         setIndex(-1);
     }
 
     return (
 
-        <div className="container">
+        <>
 
-            <div className="row mt-2">
-                <div className="col-6">
-                    <h4>Registrations for {facilityContext.selectedFacility.name}</h4>
-                </div>
-                <div className="col-6">
-                    <RegistrationDateSelector
-                        handleRegistrationDate={handleRegistrationDate}
-                        registrationDate={registrationDate}
-                    />
-                </div>
-            </div>
+            {/* List View */}
+            <Container>
 
-            { (items.length === 0) ? (
-                <div className="row mb-2">
-                    <div className="col-6">
-                        <TemplateSelector
-                            actionLabel="Generate"
-                            handleAction={handleGenerate}
-                         />
-                    </div>
-                </div>
-            ) : (
-                <span/>
-            )}
+                <Row className="row mt-2 mb-2">
+                    <Col className="col-6">
+                        <strong>Registrations for {facilityContext.selectedFacility.name}</strong>
+                    </Col>
+                    <Col className="col-5">
+                        <RegistrationDateSelector
+                            handleRegistrationDate={handleRegistrationDate}
+                            registrationDate={registrationDate}
+                        />
+                    </Col>
+                </Row>
 
-            <div className="row">
-                <div className="col-5">
-                    <List
-                        fields={["matNumberAndFeatures", "guest.firstName", "guest.lastName", "guest.comments"]}
-                        headers={["Mat", "First Name", "Last Name", "Guest Comments"]}
-                        index={index}
-                        items={items}
-                    />
-                </div>
-                <div className="col-7">
-                    Reserved for details or something
-                </div>
-            </div>
+                { (registrations.length === 0) ? (
+                    <Row className="mb-2">
+                        <Col className="col-6 ml-2">
+                            <TemplateSelector
+                                actionLabel="Generate"
+                                handleAction={handleGenerate}
+                            />
+                        </Col>
+                    </Row>
+                ) : (
+                    <span/>
+                )}
 
-        </div>
+                <Row>
+                    <Col>
+                        <List
+                            fields={["matNumberAndFeatures", "guest.firstName",
+                                "guest.lastName", "paymentType", "paymentAmount",
+                                "showerTime", "wakeupTime", "comments"]}
+                            headers={["Mat", "First Name",
+                                "Last Name", "$$", "Amount",
+                                "Shower", "Wakeup", "Comments"]}
+                            index={index}
+                            items={registrations}
+                        />
+                    </Col>
+                </Row>
+
+            </Container>
+
+        </>
 
     );
 
